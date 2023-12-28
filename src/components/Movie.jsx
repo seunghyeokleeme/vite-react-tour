ad44b717887ba5ada120a052e9dbd0436f76da38
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-12-28 22:52:25 +0900

    7.4 Movie App part Two (10:58)

---
+++ src/components/Movie.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+function Movie({ coverImg, title, year, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <span>year:{year}</span>
+      <p>{summary === "" ? "nothing" : summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
