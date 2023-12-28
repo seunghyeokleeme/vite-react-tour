a92d0531bb4670a26d6f9583d30f44a69bebf36c
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-12-28 22:18:47 +0900

    7.3 Movie App part One (16:13)

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [getCoin, setGetCoin] = useState(0);
-  const [amount, setAmount] = useState(0);
-  const [coinName, setCoinName] = useState("Btc");
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  const selectCoin = (event) => {
-    const {
-      target: { value: index },
-    } = event;
-    if (index === "-1") {
-      setGetCoin(0);
-      return;
-    }
-    setGetCoin(coins[index].quotes.USD.price / coins[0].quotes.USD.price);
-    setCoinName(coins[index].id);
-    setAmount(0);
-  };
-
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <select onChange={selectCoin}>
-            <option key="-1" value="-1">
-              select coin
-            </option>
-            {coins.map((coin, index) => (
-              <option key={index} value={index}>
-                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <hr />
-          {getCoin !== 0 ? (
-            <div>
-              <label htmlFor="btc">Btc:</label>
-              <input
-                id="btc"
-                onChange={onChange}
-                value={amount}
-                type="number"
-                placeholder="btc"
-              />
-              <div>
-                <strong>
-                  {coinName}:{amount * getCoin}개
-                </strong>
-              </div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <span>year:{movie.year}</span>
+              <p>{movie.summary === "" ? "nothing" : movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
             </div>
-          ) : null}
+          ))}
         </div>
       )}
     </div>
