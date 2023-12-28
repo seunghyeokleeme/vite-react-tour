695a824b20ec2dda9a9faa498dbc3fb7522c0733
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-12-28 21:34:48 +0900

    coin tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [getCoin, setGetCoin] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [coinName, setCoinName] = useState("Btc");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const selectCoin = (event) => {
+    const {
+      target: { value: index },
+    } = event;
+    if (index === "-1") {
+      setGetCoin(0);
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+    setGetCoin(coins[index].quotes.USD.price / coins[0].quotes.USD.price);
+    setCoinName(coins[index].id);
+    setAmount(0);
+  };
+
+  const onChange = (event) => {
+    setAmount(event.target.value);
   };
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Wrote your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((todo, idx) => (
-          <li key={idx}>{todo}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={selectCoin}>
+            <option key="-1" value="-1">
+              select coin
+            </option>
+            {coins.map((coin, index) => (
+              <option key={index} value={index}>
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <hr />
+          {getCoin !== 0 ? (
+            <div>
+              <label htmlFor="btc">Btc:</label>
+              <input
+                id="btc"
+                onChange={onChange}
+                value={amount}
+                type="number"
+                placeholder="btc"
+              />
+              <div>
+                <strong>
+                  {coinName}:{amount * getCoin}개
+                </strong>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 }
