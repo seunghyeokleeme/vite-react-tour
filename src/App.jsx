cdef7a7e11e8f971b4ab5aa53bb302f39959e011
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-12-28 18:32:52 +0900

    6.2 Deps (12:03)

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import { useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("i run only once");
   }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
